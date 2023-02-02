ba97cee4e4cfe0fdf0c09150dd6ad78bf67bbe88
nenelontano <irene.lontano@gmail.com>
2023-02-02 23:35:52 +0100

    main camera
    messo il "bordo" alla main camera ma vedi definizione personaggi e c'è qualcosa che non va nello spostamento (1.3)

---
+++ DrLaytonRM3/Assets/Scripts/CameraMotor.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotor : MonoBehaviour
+{
+    public Transform lookAt;     //è il personaggio(per ora)
+    public float boundX = 0.15f;
+    public float boundY = 0.05f;
+
+    private void LateUpdate() {
+        Vector3 delta = Vector3.zero;
+
+        //questo è per controllare se siamo dentro i limiti dell'asse x
+        float deltaX = lookAt.position.x - transform.position.x;
+        if(deltaX > boundX || deltaX < -boundX) {
+            if(transform.position.x < lookAt.position.x) {
+                delta.x = deltaX - boundX;
+            }
+            else {
+                delta.x = deltaX + boundX;
+            }
+
+        }
+
+         //questo è per controllare se siamo dentro i limiti dell'asse y
+        float deltaY = lookAt.position.y - transform.position.y;
+        if(deltaY > boundY || deltaY < -boundY) {
+            if(transform.position.y < lookAt.position.y) {
+                delta.y = deltaY - boundY;
+            }
+            else {
+                delta.y = deltaY + boundY;
+            }
+
+        }
+
+        transform.position += new Vector3(delta.x, delta.y, 0);
+    }
+}
